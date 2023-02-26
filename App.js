b7e587ca469a87541cc91b5ab909e39c8c18421d
Alejandro Ching <alejandroching2004@hotmail.com>
2023-02-26 15:56:54 -0500

    Learning CSS in Native

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
   return (
-    <View style={{
-      flex: 1, justifyContent: 'center', alignItems: 'center',
-    }}>
-      <Text style={{ fontSize: 30 }}>Hello world!!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello world!!</Text>
     </View >
   );
 }
 
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "darkgoldenrod" },
+  title: { fontSize: 30, color: "#fff" },
+});
 export default App;
