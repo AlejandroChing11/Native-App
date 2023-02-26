ba34fc37a9a813af140c01b23dcb3d9c9376874e
Alejandro Ching <alejandroching2004@hotmail.com>
2023-02-26 18:05:06 -0500

    Learning images in Native

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello world!!</Text>
+      <Text style={styles.title}>Er diavlo loko 👺👺👺</Text>
+      <Image source={{ uri: 'https://picsum.photos/200/200' }}
+        style={styles.image}
+      />
     </View >
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "darkgoldenrod" },
-  title: { fontSize: 30, color: "#fff" },
+  container: {
+    flex: 1, justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "aliceblue"
+
+  },
+  title: {
+    fontSize: 30, color: "crimson"
+  },
+  image: {
+    height: 200,
+    width: 200
+  }
 });
 export default App;
